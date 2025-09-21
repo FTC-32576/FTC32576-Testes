c83aa33407d9b07922b8f12e46d59262a37d4385
FTC All Might <allmight.ftc@gmail.com>
2025-09-21 20:12:10 -0300

    Ajustes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDConstants.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public class PIDConstants {
+    public static double Kp = 1.8;
+    public static double Ki = 0.0;
+    public static double Kd = 0.031;
+}
