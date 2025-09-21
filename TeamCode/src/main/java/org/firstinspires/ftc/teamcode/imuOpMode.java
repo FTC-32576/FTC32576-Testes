17b752b739c5104e98b75aab43849bdeaba153c2
Nobre <nobrecoding@gmail.com>
2025-09-21 20:06:40 -0300

    Ajustes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imuOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/imuOpMode.java
@@ -10,32 +10,22 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name = "imuTestes")
-public class imuOpMode extends OpMode  {
+public class imuOpMode extends OpMode {
     BNO055IMU imu;
-
-    double Kp = PIDConstants.Kp;
-
     Orientation angles;
     @Override
     public void init() {
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.loggingEnabled = false;
         imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
-
+        Orientation angles = imu.getAngularOrientation(
+                AxesReference.INTRINSIC, // ângulos medidos em relação ao robô
+                AxesOrder.ZYX, // Ordem em que os valores sao retornados; FIRST ANGLE = Heading; Second Angle = Pitch; Third Angle = Roll
+                AngleUnit.DEGREES // Unidade usada na medida no angulo - no caso, angulos. Pode usar radianos
+        );
     }
 
 
     @Override
     public void loop() {
-
-        angles = imu.getAngularOrientation(
-                AxesReference.INTRINSIC, // ângulos medidos em relação ao robô
-                AxesOrder.ZYX, // Ordem em que os valores sao retornados; FIRST ANGLE = Heading; Second Angle = Pitch; Third Angle = Roll
-                AngleUnit.DEGREES // Unidade usada na medida no angulo - no caso, angulos. Pode usar radianos
-        );
 
         telemetry.addData("IMU Heading", angles.firstAngle);
         telemetry.addData("IMU Pitch", angles.secondAngle);
